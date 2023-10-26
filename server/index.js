b5606a127d4a42951062eef928d4420288774762
MileyCao <mileyyc120@gmail.com>
2023-10-26 14:46:10 -0600

    added kpi data and route

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-// import kpiRoutes from "./routes/kpi.js";
+import kpiRoutes from "./routes/kpi.js";
 // import productRoutes from "./routes/product.js";
 // import transactionRoutes from "./routes/transaction.js";
-// import KPI from "./models/KPI.js";
+import KPI from "./models/KPI.js";
 // import Product from "./models/Product.js";
 // import Transaction from "./models/Transaction.js";
-// import { kpis, products, transactions } from "./data/data.js";
+import { kpis } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // /* ROUTES */
-// app.use("/kpi", kpiRoutes);
+app.use("/kpi", kpiRoutes);
 // app.use("/product", productRoutes);
 // app.use("/transaction", transactionRoutes);
 
